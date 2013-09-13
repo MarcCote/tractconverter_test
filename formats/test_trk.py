28eaa148608469509a18a6b3fa2599c79b57a7b9
Marc-Alexandre Cote <marc.cote.19@gmail.com>
2013-09-13 10:11:47 -0400

    Added new data file for testing. Added tests checking loading empty TRK and TCK.

---
+++ formats/test_trk.py
@@ -0,0 +1,35 @@
+from pdb import set_trace as dbg
+
+import os
+import tempfile
+import hashlib
+import numpy as np
+from functools import partial
+
+from numpy.testing import (assert_equal,
+                           assert_almost_equal,
+                           assert_array_equal,
+                           assert_array_almost_equal,
+                           assert_raises)
+
+import tractconverter
+from tractconverter import FORMATS
+
+DATA_DIR = os.path.join(os.path.realpath(tractconverter.__path__[0]), "..", "tests", "data")
+
+
+def test_load_file():
+    #Load empty file
+    filename = os.path.join(DATA_DIR, "empty.trk")
+    in_format = tractconverter.FORMATS['trk']
+    for i in in_format(filename): pass  # Check if we can iterate throught the streamlines.
+
+    #Load binary file
+    filename = os.path.join(DATA_DIR, "uncinate.trk")
+    in_format = tractconverter.FORMATS['trk']
+    in_format(filename)
+    for i in in_format(filename): pass  # Check if we can iterate throught the streamlines.
+    
+
+if __name__ == "__main__":
+    test_load_file()
