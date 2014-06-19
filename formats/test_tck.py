a19fd986f65279e4c9c3cb6212796b5ca9c1013b
Marc-Alexandre Cote <marc.cote.19@gmail.com>
2014-06-19 16:19:25 -0400

    Test dynamically adding streamlines to a file for TCK and TRK.

--- formats/test_tck.py
+++ formats/test_tck.py
@@ -14,8 +14,32 @@ from numpy.testing import (assert_equal,
 
 import tractconverter
 from tractconverter import FORMATS
+from tractconverter.formats.header import Header as H
 
 DATA_DIR = os.path.join(os.path.realpath(tractconverter.__path__[0]), "..", "tests", "data")
+
+
+def test_write_file():
+    f, filename = tempfile.mkstemp('_empty.tck')
+    os.remove(filename)
+
+    tck = FORMATS['tck']
+    tck_file = tck.create(filename)
+
+    assert_equal(len(tck(filename).load_all()), 0)
+    assert_equal(tck_file.hdr[H.NB_FIBERS], 0)
+
+    # Add one streamline to the file.
+    tck_file += [np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype='<f4')]
+    assert_equal(len(tck(filename).load_all()), 1)
+    assert_equal(tck_file.hdr[H.NB_FIBERS], 1)
+
+    # Add two more streamlines to the file.
+    tck_file += [np.array([[1, 2, 3], [4, 5, 6]], dtype='<f4'),
+                 np.array([[7, 8, 9], [0, 0, 0], [1, 1, 1]], dtype='<f4')]
+
+    assert_equal(len(tck(filename).load_all()), 3)
+    assert_equal(tck_file.hdr[H.NB_FIBERS], 3)
 
 
 def test_load_file():
@@ -29,7 +53,7 @@ def test_load_file():
     in_format = tractconverter.FORMATS['tck']
     in_format(filename)
     for i in in_format(filename): pass  # Check if we can iterate throught the streamlines.
-    
+
 
 if __name__ == "__main__":
     test_load_file()
